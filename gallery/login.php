bce4b95972ac99b4cde8615ed7a13a6aa12e421a
Gabriela Burlakova <gabriela.burlakova@gmail.com>
2014-08-22 15:10:07 +0300

    Added first files

---
+++ gallery/login.php
@@ -0,0 +1,22 @@
+<?php
+include 'common.php';
+
+if($_SESSION['is_logged'] !== true){
+    if($_POST['log_post'] == 1){
+        $name = addslashes(trim($_POST['login']));
+        $pass = trim($_POST['pass']);
+        if(strlen($name) > 3 && strlen($pass) > 3){
+            $rs = run_q('SELECT * FROM users WHERE login="' .$name.'" AND password="' . md5($pass).'"');
+            if(mysql_num_rows($rs)){
+                $row = mysql_fetch_assoc($rs);
+                $_SESSION['is_logged'] = true;
+                $_SESSION['user_id'] = $row['user_id'];
+                $_SESSION['login'] = $row['login'];
+                header('Location: index_logged.php');
+                exit;
+            }
+        }
+    }
+}
+header('Location: index.php');
+exit;
