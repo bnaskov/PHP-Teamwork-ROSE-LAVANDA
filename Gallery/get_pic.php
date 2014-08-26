8aabda47528b1b9b687776e1817e31a115a7f836
Gabriela Burlakova <gabriela.burlakova@gmail.com>
2014-08-26 13:29:01 +0300

    Fixed show public pictures

--- Gallery/get_pic.php
+++ Gallery/get_pic.php
@@ -4,26 +4,34 @@
     {
         $pic_id = (int)$_GET['pic_id'];
         $is_full = (int)$_GET['full_size'];
-
+        $user_id = 0;
 
         if($pic_id > 0)
         {
+            if(isset($_GET['getPublic'])){
+                $user_id = mysql_fetch_assoc(run_q('SELECT uc.user_id FROM pictures as p, user_catalogues as uc
+            WHERE p.pic_id='.$pic_id.' AND p.is_public=1 AND uc.catalogue_id=p.catalogue_id'));
+                $user_id = $user_id['user_id'];
+            } else {
+                $user_id = $_SESSION['user_id'];
+            }
+
             $rs = run_q('SELECT p.pic_name FROM pictures as p, user_catalogues as uc WHERE
-            p.pic_id='.$pic_id.' AND p.catalogue_id=uc.catalogue_id AND uc.user_id='.$_SESSION['user_id']);
+            p.pic_id='.$pic_id.' AND p.catalogue_id=uc.catalogue_id AND uc.user_id='.$user_id);
             $row = mysql_fetch_assoc($rs);
 
             if(strlen($row['pic_name']) > 2 &&
-            file_exists('user_pics'.DIRECTORY_SEPARATOR.$_SESSION['user_id'].DIRECTORY_SEPARATOR.$row['pic_name']))
+            file_exists('user_pics'.DIRECTORY_SEPARATOR.$user_id.DIRECTORY_SEPARATOR.$row['pic_name']))
             {
                 $fileToRead = '';
                 if($is_full)
                 {
-                    $fileToRead = 'user_pics'.DIRECTORY_SEPARATOR.$_SESSION['user_id'].DIRECTORY_SEPARATOR.$row['pic_name'];
+                    $fileToRead = 'user_pics'.DIRECTORY_SEPARATOR.$user_id.DIRECTORY_SEPARATOR.$row['pic_name'];
                     readfile($fileToRead);
                 }
                 else
                 {
-                    $fileToRead = 'user_pics'.DIRECTORY_SEPARATOR.$_SESSION['user_id'].DIRECTORY_SEPARATOR.'thumb_'.$row['pic_name'];
+                    $fileToRead = 'user_pics'.DIRECTORY_SEPARATOR.$user_id.DIRECTORY_SEPARATOR.'thumb_'.$row['pic_name'];
                     readfile($fileToRead);
                 }
             }
