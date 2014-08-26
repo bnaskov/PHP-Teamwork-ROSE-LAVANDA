f4da867591130d560f8413df323c0976416e9317
Gabriela Burlakova <gabriela.burlakova@gmail.com>
2014-08-26 12:33:25 +0300

    Fixed register.php - now adds new users correctly

--- Gallery/register.php
+++ Gallery/register.php
@@ -4,7 +4,8 @@ include 'common.php';
         $username =  mysql_real_escape_string(addslashes(trim($_POST['username'])));
         $email = $_POST['user-mail'];
         $password =  mysql_real_escape_string(md5($_POST['pass']));
-        $query = "INSERT INTO users (login, pass, email) VALUES ('$username', 'md5($pass)', '$email')";
+        $date_reg = time();
+        $query = "INSERT INTO users (login, pass, email, date_reg) VALUES ('$username', '$password', '$email', '$date_reg')";
         $result = run_q($query);
         if($result){
             $msg = "User Created Successfully.";
